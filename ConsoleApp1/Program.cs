c1d125a707155bdcbbf7b7fc878228645686b9b5
Jean Carlos Uzho <jeancarlos@gmail.com>
2026-02-25 21:58:40 -0500

    Primer commit: proyecto base ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ingresa tu edad: ");
+string texto = Console.ReadLine();
+
+int edad = int.Parse(texto);
+
+if (edad >= 18)
+{
+    Console.WriteLine("Eres mayor de edad");
+}
+else
+{
+    Console.WriteLine("Eres menor de edad");
+}
+
+Console.WriteLine("Presiona ENTER para salir...");
+Console.ReadLine();
